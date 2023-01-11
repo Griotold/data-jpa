1f1f35411919d9bfaf8142866d793c7d0eae5f30
Griotold <gotjd9773@naver.com>
2023-01-11 16:32:32 +0900

    공통 인터페이스 기능 - 순수 JPA 리파지토리 만들기

--- src/test/java/study/datajpa/repository/MemberJPARepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJPARepositoryTest.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest // 스프링부트 2.2이상 + junit5 조합이면 이거만 있으면 됨.
@@ -29,5 +33,35 @@ class MemberJPARepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD()throws Exception {
+        // given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJPARepository.save(member1);
+        memberJPARepository.save(member2);
+        // when
+        // 1. 단건 조회 검증
+        Member findMember1 = memberJPARepository.findById(member1.getId()).get();
+        Member findMember2 = memberJPARepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 2. 리스트 조회
+        List<Member> all = memberJPARepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 3. 카운트 메소드
+        long count = memberJPARepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 4. 삭제 메소드
+        memberJPARepository.delete(member1);
+        long countAfterDelete = memberJPARepository.count();
+        assertThat(countAfterDelete).isEqualTo(1);
+
+        // then
     }
 }
