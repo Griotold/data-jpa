b71d4b0804cdf9fc21bc3bd0d063cc0513fea6ce
Griotold <gotjd9773@naver.com>
2023-01-12 11:53:19 +0900

    공통 인터페이스 기능 - 설정, 적용, 분석

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -31,5 +32,35 @@ class MemberRepositoryTest {
         assertThat(findedMember).isEqualTo(member);
     }
 
+    // MemberJPARepository에서 한 거 그대로 복붙해서 잘 되는지 확인
+    @Test 
+    public void basicCRUD() throws Exception {
+        // given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        // when
+        // 1. 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 2. 리스트 조회
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 3. 카운트 메소드
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 4. 삭제 메소드
+        memberRepository.delete(member1);
+        long countAfterDelete = memberRepository.count();
+        assertThat(countAfterDelete).isEqualTo(1);
+
+        // then
+    }
 
 }
